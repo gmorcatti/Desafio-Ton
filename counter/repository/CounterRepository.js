fc4542c0e3529f7d766348479bf453b4346d5e53
Gabriel Morcatti <gabriel.morcatti@yahoo.com.br>
2021-11-15 08:49:14 -0300

    feat: Criação do Counter Repository
    Criação do repositório da funcionalidade de contador, com as funções de hit e getCurrentCounter

---
+++ counter/repository/CounterRepository.js
@@ -0,0 +1,13 @@
+const axios = require('axios');
+
+class CounterRepository {
+    async hit() {
+        return await axios.get(`https://api.countapi.xyz/hit/${process.env.COUNTAPI_NAMESPACE}/${process.env.COUNTAPI_KEY}`);
+    };
+    
+    async getCurrentCounter() {
+        return await axios.get(`https://api.countapi.xyz/get/${process.env.COUNTAPI_NAMESPACE}/${process.env.COUNTAPI_KEY}`);
+    };
+}
+
+module.exports = CounterRepository;
